9fd34b02f75ae69fcf8f583903f8282c79cd135b
navjot-ai-dev <navjotsingh542419@gmail.com>
2026-06-27 17:21:54 +0530

    add button

--- my-react-project/src/components/home.jsx
+++ my-react-project/src/components/home.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 
 const Home = () => {
+
+  const showalert = () =>{
+    alert('welcome to our website I hope you like it')
+  }
+
   return (
     <div>
          <section className="hero">
